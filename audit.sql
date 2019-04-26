03ec745167a1280fb3d4a7e18ed284bb3d28fa28
Michael Douchin <mdouchin@3liz.com>
2019-04-26 10:09:42 +0200

    Add sync information

--- audit.sql
+++ audit.sql
@@ -55,7 +55,8 @@ CREATE TABLE audit.logged_actions (
     action TEXT NOT NULL CHECK (action IN ('I','D','U', 'T')),
     row_data hstore,
     changed_fields hstore,
-    statement_only boolean not null
+    statement_only boolean not null,
+    sync_data jsonb NOT NULL
 );
 
 REVOKE ALL ON audit.logged_actions FROM public;
@@ -78,6 +79,7 @@ COMMENT ON COLUMN audit.logged_actions.action IS 'Action type; I = insert, D = d
 COMMENT ON COLUMN audit.logged_actions.row_data IS 'Record value. Null for statement-level trigger. For INSERT this is the new tuple. For DELETE and UPDATE it is the old tuple.';
 COMMENT ON COLUMN audit.logged_actions.changed_fields IS 'New values of fields changed by UPDATE. Null except for row-level UPDATE events.';
 COMMENT ON COLUMN audit.logged_actions.statement_only IS '''t'' if audit event is from an FOR EACH STATEMENT trigger, ''f'' for FOR EACH ROW';
+COMMENT ON COLUMN audit.logged_actions.sync_data IS 'Data used by the sync tool. origin = db name of the change, replayed_by = list of db name where the audit item has already been replayed';
 
 CREATE INDEX logged_actions_relid_idx ON audit.logged_actions(relid);
 CREATE INDEX logged_actions_action_tstamp_tx_stm_idx ON audit.logged_actions(action_tstamp_stm);
@@ -124,8 +126,12 @@ BEGIN
         current_query(),                              -- top-level query or queries (if multistatement) from client
         substring(TG_OP,1,1),                         -- action
         NULL, NULL,                                   -- row_data, changed_fields
-        'f'                                           -- statement_only
-        );
+        'f',                                          -- statement_only
+        jsonb_build_object(
+            'origin', current_setting('sync.server_from', true),
+            'replayed_by', jsonb_build_array()
+        )
+    );
 
     IF NOT TG_ARGV[0]::boolean IS DISTINCT FROM 'f'::boolean THEN
         audit_row.client_query = NULL;
